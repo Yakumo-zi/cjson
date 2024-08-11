f8326dc29eea3449e84246350cf32537491ef553
Yakumo-zi <1477717283@qq.com>
2024-08-11 16:32:43 +0800

    feat:improve keyword parse function

--- cjson.c
+++ cjson.c
@@ -16,43 +16,51 @@ static void cjson_parse_whitespace(cjson_context *c) {
   c->json = p;
 }
 
-static int cjson_parse_null(cjson_context *c, cjson_value *v) {
-  EXPECT(c, 'n');
-  if (c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l') {
-    return CJSON_PARSE_INVALID_VALUE;
+static int check_word(const char *src, const char *dst, int length) {
+  for (int i = 0; i < length; i++) {
+    if (src[i] != dst[i]) {
+      return CJSON_PARSE_INVALID_VALUE;
+    }
   }
-  c->json += 3;
-  v->type = CJSON_NULL;
   return CJSON_PARSE_OK;
 }
-static int cjson_parse_true(cjson_context *c, cjson_value *v) {
-  EXPECT(c, 't');
-  if (c->json[0] != 'r' || c->json[1] != 'u' || c->json[2] != 'e') {
-    return CJSON_PARSE_INVALID_VALUE;
+
+static int cjson_parse_keyword(cjson_context *c, cjson_value *v,
+                               cjson_type ty) {
+  int length = 0;
+  cjson_parse_result res;
+  switch (ty) {
+  case CJSON_NULL:
+    length = 4;
+    res = check_word(c->json, "null", 4);
+    break;
+  case CJSON_NUMBER:
+  case CJSON_FALSE:
+    length = 5;
+    res = check_word(c->json, "false", 5);
+    break;
+  case CJSON_TRUE:
+    length = 4;
+    res = check_word(c->json, "true", 4);
+    break;
+  case CJSON_STRUNG:
+  case CJSON_ARRAY:
+  case CJSON_OBJECT:
+    break;
   }
-  c->json += 3;
-  v->type = CJSON_TRUE;
-  return CJSON_PARSE_OK;
-}
-static int cjson_parse_false(cjson_context *c, cjson_value *v) {
-  EXPECT(c, 'f');
-  if (c->json[0] != 'a' || c->json[1] != 'l' || c->json[2] != 's' ||
-      c->json[3] != 'e') {
-    return CJSON_PARSE_INVALID_VALUE;
-  }
-  c->json += 4;
-  v->type = CJSON_FALSE;
-  return CJSON_PARSE_OK;
+  c->json += length;
+  v->type = ty;
+  return res;
 }
 
 static int cjson_parse_value(cjson_context *c, cjson_value *v) {
   switch (*c->json) {
   case 'n':
-    return cjson_parse_null(c, v);
+    return cjson_parse_keyword(c, v, CJSON_NULL);
   case 'f':
-    return cjson_parse_false(c, v);
+    return cjson_parse_keyword(c, v, CJSON_FALSE);
   case 't':
-    return cjson_parse_true(c, v);
+    return cjson_parse_keyword(c, v, CJSON_TRUE);
   case '\0':
     return CJSON_PARSE_EXPECT_VALUE;
   default:
