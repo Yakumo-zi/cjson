829f8925d0174e6f1f7029ea19dfb4704b2f7257
Yakumo-zi <1477717283@qq.com>
2024-08-11 16:06:17 +0800

    feat:add unit test support

---
+++ cjson.c
@@ -0,0 +1,6 @@
+#include "cjson.h"
+
+cjson_parse_result cjson_parse(cjson_value *v, const char *json) {
+  return CJSON_PARSE_OK;
+}
+cjson_type cjson_get_type(const cjson_value *v) { return CJSON_NULL; }
